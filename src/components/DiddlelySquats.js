3b7a5eecb2e02bdf40b9a9249c16286fee64a202
Emimint <90863470+Emimint@users.noreply.github.com>
2023-03-01 14:47:11 -0500

    create Diddlely css rules

--- src/components/DiddlelySquats.js
+++ src/components/DiddlelySquats.js
@@ -1,10 +1,12 @@
-function DiddlelySquats( props ) {
-    const thedate = props.date.toLocaleDateString();
-    return (
-      <div>
-        <p> Regarding that day (i.e. {thedate}), I did nuffin'... </p>
-      </div>
-    );
+import "./DiddlelySquats.css";
+
+function DiddlelySquats(props) {
+  const thedate = props.date.toLocaleDateString();
+  return (
+    <div className="squat">
+      <p> Regarding that day (i.e. {thedate}), I did nuffin'... </p>
+    </div>
+  );
 }
 
 export default DiddlelySquats;
