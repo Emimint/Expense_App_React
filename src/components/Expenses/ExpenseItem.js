ba64420306b08943b1ce54ef5810ad08f1fd74fb
Emimint <90863470+Emimint@users.noreply.github.com>
2023-07-26 14:07:07 -0400

    add test event handler function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,31 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import DiddlelySquats from "./DiddlelySquats";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  let ponct = "!";
+
+  const [title, setTitle] = useState(props.title); // React hook
+
+  const clickHandler = () => {
+    const newTitle = title + ponct;
+    setTitle(newTitle);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <DiddlelySquats date={props.date}></DiddlelySquats>
         <p>the key for this object is {props.id}</p>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
