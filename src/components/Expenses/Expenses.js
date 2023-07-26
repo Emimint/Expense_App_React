e3c058a679f70427f09881da18474e91c7b25fa0
Emimint <90863470+Emimint@users.noreply.github.com>
2023-07-26 16:53:19 -0400

    add ExpensesFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses({ expenses }) {
-  return <Card className="expenses">{expenses.map(ExpenseItem)}</Card>;
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+  const onYearSelection = (selection) => {
+    setSelectedYear(selection);
+    console.log(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <div>{selectedYear}</div>
+      <ExpensesFilter onYearSelection={onYearSelection} />
+      {expenses.map(ExpenseItem)}
+    </Card>
+  );
 }
 
 export default Expenses;
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "../UI/Card";
+
+import "./ExpensesFilter.css";
+
+
+/*
+Forward year to Expenses
+Store the year as a state in Expenses
+
+*/
+
+const ExpensesFilter = ({onYearSelection }) => {
+  return (
+    <Card className="expenses">
+      <div className="expenses-filter">
+        <div className="expenses-filter__control">
+          <label>Filter by year</label>
+                  <select
+            onChange={(e) => {
+              onYearSelection(e.target.value);
+            }}
+          >
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            <option value="2018">2018</option>
+          </select>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpensesFilter;
