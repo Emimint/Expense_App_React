4225ae065aa81a151bd3639953c195bd7e285f5d
Emimint <90863470+Emimint@users.noreply.github.com>
2023-07-26 17:07:35 -0400

    comment out first event handler example
    Remove since not needed here

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,30 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import DiddlelySquats from "./DiddlelySquats";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  let ponct = "!";
+  // let ponct = "!";
 
-  const [title, setTitle] = useState(props.title); // React hook
+  // const [title, setTitle] = useState(props.title); // React hook
 
-  const clickHandler = () => {
-    const newTitle = title + ponct;
-    setTitle(newTitle);
-  };
+  // const clickHandler = () => {
+  //   const newTitle = title + ponct;
+  //   setTitle(newTitle);
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        {/* <h2>{title}</h2> */}
+        <h2>{props.title}</h2>
         <DiddlelySquats date={props.date}></DiddlelySquats>
         <p>the key for this object is {props.id}</p>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      {/* <button onClick={clickHandler}>Change title</button> */}
     </Card>
   );
 };
