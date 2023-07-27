f58a8159f8587d65fb2024d9eae3ad67431cf47f
Emimint <90863470+Emimint@users.noreply.github.com>
2023-07-27 15:24:52 -0400

    add chart components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses({ expenses }) {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
@@ -13,6 +14,7 @@ function Expenses({ expenses }) {
   return (
     <Card className="expenses">
       <ExpensesFilter onYearSelection={setSelectedYear} />
+      <ExpensesChart expenses={selectedExpenses}></ExpensesChart>
       {selectedExpenses.length === 0 ? (
         <p>No expenses for the selected year</p>
       ) : (
